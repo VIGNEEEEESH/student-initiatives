471215957a843549add8f99dc4c441b774da3d4e
Harsh Morayya <harshmorayya@ymail.com>
2023-02-10 11:02:56 +0530

    Added API documentation to README.MD

--- student-initiatives-backend/src/main/java/com/woxsen/studentinitiatives/rest/UserREST.java
+++ student-initiatives-backend/src/main/java/com/woxsen/studentinitiatives/rest/UserREST.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +26,7 @@ public class UserREST {
 		this.userService = userService;
 	}
 	
-	@PutMapping(value = "/user/")
+	@PostMapping(value = "/user/")
 	public void saveUser(@RequestBody User user) {
 		System.out.println("Executed");
 		userService.save(user);
